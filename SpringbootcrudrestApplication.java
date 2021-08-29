b61bca394b2c985ca37d33ccaf48f2faf55dbf53
uic18m <46988516+uic18m@users.noreply.github.com>
2021-08-29 20:04:38 +0530

    Add files via upload

---
+++ SpringbootcrudrestApplication.java
@@ -0,0 +1,13 @@
+package com.companyname.springbootcrudrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcrudrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootcrudrestApplication.class, args);
+	}
+
+}
